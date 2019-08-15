a327887ed0f7b0846c7d43610cfe937e716e6dbc
14-10526 <14-10526@usb.ve>
2019-08-15 19:02:33 -0400

    subo el programa

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Expresiones regulares 
+            Regex name = new Regex("[A-Z][a-z][^#&<>\"~;$^%{}?+*0-9]{1,20}$");             //Nombre y Apellido
+            Regex id = new Regex("^[V|E|J|G|C]-[0-9][^a-zA-Z#&<>\"~;$^%{}?+*]");           //Cedula o RIF  
+            Regex age = new Regex(@"^\d{1,3}$");                                            //Edad 
+            Regex address = new Regex("[/w|/b|/s/#/-]+");                                  //Direccion
+
+            Regex[] expresiones = {name,name,id,age,address};
+            
+            //Chequeamos que se ingrese por consola al menos un string 
+            if(args.Length < 1) 
+            {
+                Console.WriteLine("Debe ingresar al menos un (1) archivo de texto");
+                System.Environment.Exit(1);
+            }
+            else
+            {
+                foreach(string file in args)
+                { 
+                    //Leemos archivo de texto linea por linea  
+                    int lineNumber = 1;
+                    string[] lines = File.ReadAllLines(file); 
+                    foreach(string line in lines)
+                    {
+                        string[] words = line.Split("|");
+                        if(words.Length != expresiones.Length)
+                        {
+                            Console.WriteLine("Existe un error con el numero de campos en el texto de entrada");
+                            System.Environment.Exit(1);
+                        }
+                        else
+                        {
+                            foreach(string word in words)
+                            {
+                                //aqui creo que mejor podria llevar un contador con el indice y no tener
+                                //que usar la funcion IndexOf (ver desempeno)
+                                int index = Array.IndexOf(words,word);
+                                Match match = expresiones[index].Match(words[index]);
+                                if (match.Success == false)
+                                {
+                                    index = index +1;
+                                    Console.Write(" ERROR Linea: ");
+                                    Console.Write(lineNumber);
+                                    Console.Write(" Columna: ");
+                                    Console.Write(index);
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                        lineNumber = lineNumber +1;
+                    }
+                
+                }
+            }
+        }
+    }
+}
